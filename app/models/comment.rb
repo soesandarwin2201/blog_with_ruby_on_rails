c20a210107bba54c87887985a6beb09ee043ecad
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-03-02 21:14:50 +0630

    create custom method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,9 @@
 class Comment < ApplicationRecord
+ belongs_to :user , foreign_key: 'author_id'
+ belongs_to :posts
+ has_many :likes
+
+def update_post_comments_counter
+    posts.update(commentscounter: posts.comments.count)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+ belongs_to :user, foreign_key: 'author_id'
+ has_many :likes
+ has_many :comments, dependent: :destroy
+
+ def update_user_posts_counter
+    user.update(postscounter: user.posts.count)
+  end
+
+  def recent_comments(limit = 5)
+    comments.order(created_at: :desc).limit(limit)
+  end
 end
