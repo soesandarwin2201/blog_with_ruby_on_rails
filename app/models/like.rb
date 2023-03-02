bef9b591050268abb1ec4430e864ca4c9f8e44db
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-03-02 22:25:01 +0630

    Change the private method

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,11 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :posts
 
-  def update_post_likes_counter
-    posts.update(likescounter: posts.likes.count)
-  end
+  after_save :update_post_likes_counter
+
+private
+
+def update_post_likes_counter
+  post.increment!(:likescounter)
+end
 end
