18d0f81c1270ba96cd8357d0794e497f2a803c78
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-03-03 15:51:45 +0630

    Fix the linters

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,37 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
- let(:user) { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', postscounter: 0)}
+  let(:user) { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', postscounter: 0) }
 
- before { user.save }
+  before { user.save }
 
- it 'Name should not be blank' do 
+  it 'Name should not be blank' do
+    user.name = nil
 
-  user.name = nil
-
-  expect(user).to_not be_valid
-
- end
+    expect(user).to_not be_valid
+  end
 
 
 
- it 'postcounter must be an integer greater than or equal' do
+  it 'postcounter must be an integer greater than or equal' do
+    user.postscounter = -1
 
-  user.postscounter = -1
+    expect(user).to_not be_valid
+  end
 
-  expect(user).to_not be_valid
 
+
+  it 'user should be valid' do
+    expect(user).to be_valid
+  end
 end
-
-
-
- it 'user should be valid' do 
-
-  expect(user).to be_valid
-
- end
-
-
-
-end
-
