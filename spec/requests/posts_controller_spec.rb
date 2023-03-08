26205c3bbd95dbb0c228e14ce9b6181ee923694e
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-03-08 17:18:52 +0630

    Fix the linters

--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -1,41 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :request do
- describe "GET #index" do
-    it "returns a successful response" do
-      get "/user/2/posts"
+  describe 'GET #index' do
+    it 'returns a successful response' do
+      get '/user/2/posts'
       expect(response).to be_successful
     end
 
-    it "assigns all users to @users" do 
-      get "/user/2/posts"
+    it 'assigns all users to @users' do
+      get '/user/2/posts'
       expect(assigns(:posts)).to eq(Post.all)
-  end
+    end
 
-    it "check the template" do 
-      get "/user/2/posts"
+    it 'check the template' do
+      get '/user/2/posts'
       expect(response).to render_template('index')
-  end
+    end
 
-  it "check for the content" do 
-    get "/user/2/posts"
-    expect(response.body).to include('<p>Here is the list for all post of bloger.</p>')
-  end
+    it 'check for the content' do
+      get '/user/2/posts'
+      expect(response.body).to include('<p>Here is the list for all post of bloger.</p>')
+    end
 
-  it 'check for the right id' do 
-    get "/user/2/posts/1"
-    expect(response).to be_successful
-  end
+    it 'check for the right id' do
+      get '/user/2/posts/1'
+      expect(response).to be_successful
+    end
 
-  it 'check for the right template for the given id' do 
-    get "/user/2/posts/1"
-    expect(response).to render_template('show')
-  end
+    it 'check for the right template for the given id' do
+      get '/user/2/posts/1'
+      expect(response).to render_template('show')
+    end
 
-  it 'check for the right body for the given id' do 
-    get "/user/2/posts/1"
-    expect(response.body).to include('<h1>list of posts for the given person!</h1>')
+    it 'check for the right body for the given id' do
+      get '/user/2/posts/1'
+      expect(response.body).to include('<h1>list of posts for the given person!</h1>')
+    end
   end
 end
-end
-
