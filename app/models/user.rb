f0dad6db3bba9fcbf77d498d1ba00384581beff8
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-03-15 11:06:26 +0630

    Create the header for the signin and sign out

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,15 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
 
   validates :name, presence: true
+  validates :postscounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_posts(limit = 3)
     posts.order(created_at: :desc).limit(limit)
+  end
+
+  def init
+    return unless postscounter.nil?
+
+    self.postscounter = 0
   end
 end
