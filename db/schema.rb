134bc7391f8aa7c5d131e65325692f40ce6c4dec
Nizamuddin Ahmadzai <78977308+Nizamuddin4493@users.noreply.github.com>
2023-03-15 18:42:19 +0430

    CSS for Sign up page

--- db/schema.rb
+++ db/schema.rb
@@ -36,8 +36,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_15_030906) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "text"
-    t.integer "commentscounter"
-    t.integer "likescounter"
+    t.integer "commentscounter", default: 0
+    t.integer "likescounter", default: 0
     t.integer "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_15_030906) do
     t.string "name"
     t.string "photo"
     t.text "bio"
-    t.integer "postscounter"
+    t.integer "postscounter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
@@ -56,6 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_15_030906) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
