59c15adae3739f08fece8d3b2b31e5fd0d567d8e
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-03-15 10:43:13 +0630

    Create the singup page

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,6 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
 
   validates :name, presence: true
-  validates :postscounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_posts(limit = 3)
     posts.order(created_at: :desc).limit(limit)
