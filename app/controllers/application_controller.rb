39cbc0e25a6e60b85bbe8560db7034930ff8e92c
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-03-15 21:50:54 +0630

    Fix the linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,14 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :update_allowed_parameters, if: :devise_controller?
 
-   protected
+  protected
 
   def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| 
-     u.permit(:name, :photo, :bio, :email, :password)}
-    devise_parameter_sanitizer.permit(:account_update) { |u| 
-     u.permit(:name, :photo, :bio, :email, :password, :current_password)}
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :photo, :bio, :email, :password)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :photo, :bio, :email, :password, :current_password)
+    end
   end
 end
